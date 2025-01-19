58f1e612445070af212c94c7a88dd129e1c0fb1e
Cleiton Ricardo Spagnol <101300606+cleitonricardo@users.noreply.github.com>
2025-01-19 10:40:39 -0300

    Criado a Entidade Autor

--- src/entities/Autor.ts
+++ src/entities/Autor.ts
@@ -1,5 +1,38 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+  } from "typeorm";
+
+
+  @Entity("authors")
+
 class Autor {
-    
+    @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: "varchar", length: 255, nullable: false })
+  name: string;
+
+  @Column({ type: "date", nullable: false })
+  birthdate: Date;
+
+  @Column({ type: "text", nullable: true })
+  biography: string;
+
+  @Column({ type: "varchar", length: 255, nullable: false })
+  nationality: string;
+
+  @Column({ type: "boolean", default: true })
+  active: boolean;
+
+  @CreateDateColumn({ type: "timestamp" })
+  created_at: Date;
+
+  @UpdateDateColumn({ type: "timestamp" })
+  updated_at: Date;
 }
 
 export default Autor;
